198efe608fdba341b00d869ec2ba6a21d3a94386
Alireza Alavi <alireza.alavi237@gmail.com>
2026-03-04 03:34:16 +0100

    improved code in PomodoroTimer

--- src/tui/widgets/pomodoro.py
+++ src/tui/widgets/pomodoro.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from textual.app import ComposeResult
 from textual.widgets import Static
 
@@ -8,19 +10,24 @@ from src.tui.widgets.display import TimerDisplay
 class PomodoroTimer(Static):
 	"""A self-contained Pomodoro timer widget."""
 
+	def __init__(self, *args: Any, **kwargs: Any) -> None:
+		super().__init__(*args, **kwargs)
+		self.timer_display = TimerDisplay()
+		self.timer_controls = TimerControls()
+
 	def compose(self) -> ComposeResult:
 		"""Child widgets for the component."""
-		yield TimerDisplay()
-		yield TimerControls()
+		yield self.timer_display
+		yield self.timer_controls
 
 	def on_timer_controls_started(self, message: TimerControls.Started) -> None:
 		"""Handle start button click from the internal controls."""
-		self.query_one(TimerDisplay).start()
+		self.timer_display.start()
 
 	def on_timer_controls_stopped(self, message: TimerControls.Stopped) -> None:
 		"""Handle stop button click from the internal controls."""
-		self.query_one(TimerDisplay).stop()
+		self.timer_display.stop()
 
 	def on_timer_controls_reset(self, message: TimerControls.Reset) -> None:
 		"""Handle reset button click from the internal controls."""
-		self.query_one(TimerDisplay).reset()
+		self.timer_display.reset()
